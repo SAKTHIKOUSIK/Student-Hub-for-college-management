722f83a846159d319646720d9f98dcbf9f271b85
chaituabhade <156505752+chaituabhade@users.noreply.github.com>
2024-08-30 16:50:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/schoolms", {
+    .connect("//data base url", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
